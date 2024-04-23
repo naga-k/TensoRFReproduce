a17f487386c2cdf667d63a743ba9fd3a794bc9d7
Naga Karumuri <anjaneyulun02@gmail.com>
2024-04-23 16:40:45 -0400

    ending old format here here. I will be combining tensorf_base and tensorf now since i will be only using tensorf_vm_split

--- model/extractor/tensorf.py
+++ model/extractor/tensorf.py
@@ -5,3 +5,6 @@ class TensorVMSplit(TensorBase):
     def __init__(self, aabb, gridSize, device, **kargs):
         super(TensorVMSplit, self).__init__(aabb, gridSize, device, **kargs)
         pass
+
+    def init_one_svd(self, n_component, gridSize, scale, device):
+        
